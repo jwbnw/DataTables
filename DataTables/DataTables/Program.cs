3c7c6ed1ef93bfa4c06b632c0ed1892092ef22dd
jb <belford.james@outook.com>
2016-01-31 13:43:30 -0800

    Defined user data tables, still need to populate UserDataTable with some test data.

--- DataTables/DataTables/Program.cs
+++ DataTables/DataTables/Program.cs
@@ -1,17 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace DataTables
 {
     class Program
     {
+        
+        static DataTable createUserSampleTable()
+        {
+            DataTable UserDataTable = new DataTable();
 
-        //Some global Dataset vars
+            UserDataTable.Columns.Add("StudentID", typeof(string));
+            UserDataTable.Columns.Add("Last name", typeof(string));
+            UserDataTable.Columns.Add("First name", typeof(string));
 
-        //createSampleSet(){}
+            return UserDataTable;
+           
+        }
+        
+        static DataTable createUserClassTable()
+        {
+
+            DataTable userCourseDataTable = new DataTable();
+
+            userCourseDataTable.Columns.Add("StudentID", typeof(string));
+            userCourseDataTable.Columns.Add("Course1", typeof(string));
+            userCourseDataTable.Columns.Add("Course2", typeof(string));
+            userCourseDataTable.Columns.Add("Course3", typeof(string));
+            userCourseDataTable.Columns.Add("Course4", typeof(string));
+            userCourseDataTable.Columns.Add("Course5", typeof(string));
+            userCourseDataTable.Columns.Add("Course6", typeof(string));
+
+            return userCourseDataTable;
+        }
 
         //loadSetFromCSV(){}
 
