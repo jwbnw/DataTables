285339e58f819e435846e69a1c29d3858b65f4a3
jb <belford.james@outook.com>
2016-01-30 22:53:16 -0800

    First Update

---
+++ DataTables/DataTables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This program is dedicated to building the ablity to read in .csv files, construct data-tables
+            //compare values in data tables then return certain values from the table. 
+            
+           //Going to start tomorrow beacuse I'm way to tired tonight
+        }
+    }
+}
